b0201591c803ac607bb93dc5536180b4122bd0a4
Jiya Joseph <jiyajoseph53@gmail.com>
2026-01-31 13:53:53 +0530

    Finding smallest number program

---
+++ smallest .c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+int n,m,p;
+printf("enter three numbers\n");
+scanf("%d%d%d"&n,&m,&p);
+if(n<p)
+ if(n<m)
+{
+printf("n is the smallest",n);
+}
+else
+{
+printf("m is the smallest",m);
+}
+else 
+{
+if (m<p)
+}
+printf("m is the smallest",m);
+}
+else 
+{
+printf("p is the smallest",p);
+}
+}
